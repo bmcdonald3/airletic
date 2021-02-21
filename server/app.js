3418ec10f8fb8aff14611573cc0e3b8abb90914e
Ben McDonald <mcdobe100@gmail.com>
2021-02-20 16:16:46 -0800

    add streaming and thumbnail pictures

--- server/app.js
+++ server/app.js
@@ -26,10 +26,6 @@ const videos = [
     },
 ];
 
-app.get('/video', (req, res) => {
-    res.sendFile('assets/0.mp4', { root: __dirname });
-});
-
 app.use(cors())
 app.get('/videos', (req, res) => {
     res.json(videos)
@@ -45,6 +41,41 @@ app.get('/video/:id/data', (req, res) => {
     res.json(videos[id]);
 });
 
+app.get('/video/:id', (req, res) => {
+    const path = `assets/${req.params.id}.mp4`;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
+        const chunksize = (end-start) + 1;
+        const file = fs.createReadStream(path, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
+    }
+});
+
+app.get('/video/:id/poster', (req, res) => {
+    res.sendFile(`assets/${req.params.id}.png`, { root: __dirname });
+});
+
 app.listen(4000, () => {
-    console.log('Listening on port 4000')
+    console.log('Listening on port 4000');
 });
